7506f5e4c94392a979bf8b66d9be30b130e7558b
LuthoYRN <luthoyrn@gmail.com>
2025-03-07 22:51:50 +0200

    reworked codebase

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,47 +3,43 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
-    [SerializeField]private float speed;
-    private Animator anim;
-    private bool grounded;
-    private void Awake()
+    [SerializeField] public float speed;
+    [SerializeField] public float jumpHeight; 
+    private float horizontalInput;
+    private bool facingRight = true;
+ 
+    void Awake()
     {
-        //Grab reference for rigidbody and animator frm object
         body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.linearVelocity = new Vector2(horizontalInput*speed,body.linearVelocity.y);
-        
-        //Flip player when moving left or right
-        if (horizontalInput>0.01f)
-        {
-            transform.localScale = new Vector3(2,2,2);
-            
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //facing left or right
+        if (horizontalInput<0f && facingRight == true){
+            transform.eulerAngles = new Vector3(0f,-180f ,0f);
+            facingRight= false;
         }
-        else if (horizontalInput<-0.01f){
-            transform.localScale = new Vector3(-2,2,2);
-        }
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        else if (horizontalInput>0f && facingRight == false){
+            transform.eulerAngles = new Vector3(0f,0f ,0f);
+            facingRight= true;
+            }
+
+        if (Input.GetKey(KeyCode.Space))
         {
             Jump();
         }
-        anim.SetBool("run",horizontalInput!=0);
-        anim.SetBool("grounded",grounded);
     }
-    private void Jump(){
-        body.linearVelocity = new Vector2(body.linearVelocity.x,speed/2);
-        anim.SetTrigger("jump");
-        grounded = false;
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    void FixedUpdate()
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            grounded = true;
-        }
+        body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocityY); //move horizontal
+    }
+
+    void Jump(){
+        body.linearVelocity = new Vector2(body.linearVelocityX, jumpHeight) ;
     }
 }
