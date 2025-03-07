88f13a5b005949251f227279c6c45d25db74ebd1
LuthoYRN <luthoyrn@gmail.com>
2025-03-08 01:25:18 +0200

    Updated Player controller and movement script to change animation conditions and add fall detection.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
+    private Animator anim;
     [SerializeField] public float speed;
     [SerializeField] public float jumpHeight; 
     private float horizontalInput;
@@ -11,12 +12,13 @@ public class PlayerMovement : MonoBehaviour
     void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-    }
+        anim = GetComponent<Animator>();
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal"); //movement
 
         //facing left or right
         if (horizontalInput<0f && facingRight == true){
@@ -30,8 +32,19 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space) && isGrounded == true)
         {
+
             Jump();
-            isGrounded = false;
+        }
+
+        if (Mathf.Abs(horizontalInput)>0.1f){ //check running
+            anim.SetFloat("run",1f);
+        }else if (Mathf.Abs(horizontalInput)<0.1f) anim.SetFloat("run",0f);
+
+        //checking when the player is falling
+        if (body.linearVelocityY < 0f && isGrounded == false)
+        {
+            anim.SetBool("fall", true);
+            anim.SetBool("jump",false);
         }
     }
 
@@ -41,13 +54,17 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Jump(){
+        isGrounded = false;
+        anim.SetBool("jump",true);
         body.linearVelocity = new Vector2(body.linearVelocityX, jumpHeight) ;
     }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
             isGrounded = true;
+            anim.SetBool("fall",false);
         }
     }
 }
