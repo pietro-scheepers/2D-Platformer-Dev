b598194cd8d288671c1fe729bd63aa5a6de4d6ff
LuthoYRN <luthoyrn@gmail.com>
2025-03-25 22:15:49 +0200

    Added meele attack abilities to all enemy types

--- Assets/Scripts/Enemy/RangedEnemy.cs
+++ Assets/Scripts/Enemy/RangedEnemy.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
-public class MeleeEnemy : MonoBehaviour
+public class RangedEnemy : MonoBehaviour
 {
     [Header("Attack Parameters")]
     [SerializeField] private float attackCooldown;
     [SerializeField] private int _damage;
     [SerializeField] private float range;
+
+    [Header("RangedAttack")]
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] projectiles;
     
     [Header("Collider Parameters")]
     [SerializeField] private float colliderDistance;
@@ -14,16 +18,15 @@ public class MeleeEnemy : MonoBehaviour
     [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
-    
     //references
     private Animator anim;
     private Health playerHealth;
     private EnemyPatrol enemyPatrol;
+
     private void Awake(){
         anim = GetComponent<Animator>();
         enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
-
     private void Update()
     {
         cooldownTimer+= Time.deltaTime;
@@ -31,41 +34,38 @@ public class MeleeEnemy : MonoBehaviour
         {
             if (cooldownTimer >= attackCooldown){
                 cooldownTimer = 0;
-                int attack = Random.Range(0, 2);
-                if (attack==0)
-                {
-                    anim.SetTrigger("meeleAttack1");
-                }
-                else{
-                    anim.SetTrigger("meeleAttack2");
-                }
-                
+                anim.SetTrigger("rangedAttack");
             }   
         }
         if (enemyPatrol!=null){
                 enemyPatrol.enabled = !PlayerInSight();
         }
     }
+    private void RangedAttack(){
+        cooldownTimer = 0;
+        projectiles[FindProejctile()].transform.position = firePoint.position;
+      //  projectiles[FindProejctile()].GetComponent<EnemyProjectile>().ActivateProjectile();
+    }
+    private int FindProejctile(){
+        for (int i = 0; i < projectiles.Length; i++)
+        {
+            if (!projectiles[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
     private bool PlayerInSight(){
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center+transform.right*range*transform.localScale.x*colliderDistance,
         new Vector3(boxCollider.bounds.size.x*range,boxCollider.bounds.size.y,boxCollider.bounds.size.z)
         ,0,Vector2.left,0,playerLayer);
-
-        if (hit.collider != null){
-            playerHealth = hit.transform.GetComponent<Health>();
-        }
         return hit.collider!=null;
     }
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(boxCollider.bounds.center+transform.right*range*transform.localScale.x*colliderDistance,
         new Vector3(boxCollider.bounds.size.x*range,boxCollider.bounds.size.y,boxCollider.bounds.size.z));
-    }
-
-    private void DamagePlayer(){
-        if (PlayerInSight()){
-            playerHealth.TakeDamage(_damage);
-        }
     }
 }
