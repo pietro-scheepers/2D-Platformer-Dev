0471b52ce0933fb9799d056085da24538b421525
pietro-scheepers <pietroscheepers@gmail.com>
2025-03-27 10:57:05 +0200

    Added view map functionality to the main menu
    The map will be updated according to the levels that the player have completed

--- Assets/Scripts/Menu/mainMenu.cs
+++ Assets/Scripts/Menu/mainMenu.cs
@@ -3,7 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-   public void playGame ()
+
+    public GameObject mainMenuPanel;
+    public GameObject mapPanel;
+    public GameObject[] mapPieces;
+    // Simulate player's progress - GET FROM SAVE FILE
+    public int levelsCompleted = 0;
+    public void playGame ()
     {
         Debug.Log("Play button pressed");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Make sure to update this to say +1 whenever a level is finished!!!!!!
@@ -13,5 +19,28 @@ public class mainMenu : MonoBehaviour
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void OpenMap()
+    {
+        mainMenuPanel.SetActive(false);
+        mapPanel.SetActive(true);
+        UpdateMap();
+    }
+
+    public void CloseMap()
+    {
+        mapPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    void UpdateMap()
+    {
+        //for (int i = 0; i < mapPieces.Length; i++)
+        //{
+        //  mapPieces[i].SetActive(i <= levelsCompleted);
+        //}
+        Debug.Log("Levels completed: " + levelsCompleted);
+        mapPieces[levelsCompleted].SetActive(true);
     }
 }
